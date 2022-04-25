c8069fcb07b6b709e5c1af278c71888783bc3ef5
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-25 18:20:12 +0200

    Finish yahtzee, create dad jokes project

---
+++ dad-jokes/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import JokeList from "./dad-jokes/JokeList";
+
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
