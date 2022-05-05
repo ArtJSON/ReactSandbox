114ffe0933346595b61ded4091a6acca6f315b25
ArtJSON <artur.pietrzak1@outlook.com>
2022-05-06 01:20:30 +0200

    First commit dog project

---
+++ dogs/src/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    console.log(this.props.links);
+
+    return (
+      <nav className="navbar">
+        <div className="navbar-main">
+          <h1 className="navbar-title">Artur Pietrzak</h1>
+          <i
+            onClick={this.props.handleClick}
+            className={this.props.expanded ? "fas fa-times" : " fas fa-bars"}
+          ></i>
+        </div>
+        <ul
+          className={`navbar-options ${
+            this.props.expanded ? "navbar-options-expand" : ""
+          }`}
+        >
+          {this.props.links.map((l, index) => {
+            return (
+              <li key={index}>
+                <Link to={l.url}>{l.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
