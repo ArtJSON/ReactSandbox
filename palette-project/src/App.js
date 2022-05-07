a054712bf36f40d2df6d809b227668b5f89491fc
ArtJSON <artur.pietrzak1@outlook.com>
2022-05-07 21:59:59 +0200

    Start creating palette project

---
+++ palette-project/src/App.js
@@ -0,0 +1,12 @@
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[5]} />
+    </div>
+  );
+}
+
+export default App;
---
+++ palette-project/src/Palette.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Palette() {
+  return (
+    <div className="palette">
+      <div className="palette-colors"></div>
+    </div>
+  );
+}
