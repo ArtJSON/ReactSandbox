81a1be02f8257cf6ffaa1131c18ae58356a5bc6f
ArtJSON <artur.pietrzak1@outlook.com>
2022-05-08 22:57:43 +0200

    Add overlay for copying

--- palette-project/src/ColorBox.js
+++ palette-project/src/ColorBox.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./styles/ColorBox.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function ColorBox(props) {
   return (
-    <div className="color-box" style={{ background: props.background }}>
-      <div className="copy-container">
-        <div className="box-content">
-          <span>{props.name}</span>
+    <CopyToClipboard text={props.background}>
+      <div className="color-box" style={{ background: props.background }}>
+        <div
+          className="copy-overlay"
+          style={{ background: props.background }}
+        ></div>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{props.name}</span>
+          </div>
+          <button className="btn-copy">Copy</button>
         </div>
-        <button className="btn-copy">Copy</button>
+        <span className="see-more">More</span>
       </div>
-      <span className="see-more">More</span>
-    </div>
+    </CopyToClipboard>
   );
 }
