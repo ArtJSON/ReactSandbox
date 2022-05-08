2e2e441c39541689b074f25ecb90d203533ce643
ArtJSON <artur.pietrzak1@outlook.com>
2022-05-08 23:37:32 +0200

    Finish styling copy animation etc

--- palette-project/src/ColorBox.js
+++ palette-project/src/ColorBox.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./styles/ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useState } from "react";
 
 export default function ColorBox(props) {
+  const [copied, setCopied] = useState(false);
+
+  const changeCopyState = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+
   return (
-    <CopyToClipboard text={props.background}>
+    <CopyToClipboard text={props.background} onCopy={changeCopyState}>
       <div className="color-box" style={{ background: props.background }}>
         <div
-          className="copy-overlay"
+          className={`copy-overlay ${copied ? "show" : ""}`}
           style={{ background: props.background }}
         ></div>
+        <div className={`copy-msg ${copied ? "show" : ""}`}>
+          <h2>Copied!</h2>
+          <p>{props.background}</p>
+        </div>
         <div className="copy-container">
           <div className="box-content">
             <span>{props.name}</span>
